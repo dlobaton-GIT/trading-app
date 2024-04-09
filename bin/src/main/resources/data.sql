827cf0f91e515fb93cf22dddf026271cf03663d7
dlobaton <71184207+dlobaton@users.noreply.github.com>
2024-04-09 10:05:26 +0200

    Delete bin directory

--- bin/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Security(id, name) VALUES(1, 'WSB');
-INSERT INTO Users(id, username, password) VALUES(1, 'Diamond', 'admin');
-INSERT INTO Users(id, username, password) VALUES(2, 'Paper', 'admin');
-INSERT INTO Users(id, username, password) VALUES(3, 'Iron', 'admin');
-
